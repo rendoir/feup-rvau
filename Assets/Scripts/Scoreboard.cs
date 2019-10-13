8e219f166c8c7221c5efd076377bd87df3ea1fa3
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-10-14 00:54:56 +0100

    Added A Personal Scene and Create A Very Basic Scoreboard

---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour
+{
+    public GameObject ScoreDisplay;
+    public string BaseText = "Score:";
+
+    private TextMesh TextRenderer;
+
+    private long CurrentScore = 0;
+
+    void Start()
+    {
+        this.TextRenderer = ScoreDisplay.GetComponent<TextMesh>();
+        this.SetScoreText();
+    }
+
+    private void SetScoreText()
+    {
+        this.TextRenderer.text = BaseText + " " + this.CurrentScore;
+    }
+
+    public void IncreaseScore(long delta = 1)
+    {
+        this.CurrentScore += delta;
+        this.SetScoreText();
+    }
+
+    public void DecreaseScore(long delta=1)
+    {
+        this.CurrentScore -= delta;
+        this.SetScoreText();
+    }
+
+    public long GetCurrentScore()
+    {
+        return this.CurrentScore;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            this.IncreaseScore();
+        }
+    }
+}
