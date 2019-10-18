67271e4067cbed6d1e410e294461445530299c93
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-10-18 22:26:24 +0100

    initial draft of the knockdownable mechanic for the throwing minigame

---
+++ Assets/Scripts/Knockable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockable : MonoBehaviour
+{
+    public float deltaE = 5;
+
+    private Quaternion initialQuarternion;
+    private Renderer objectRenderer;
+
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+        initialQuarternion = this.gameObject.transform.rotation;
+    }
+    void FixedUpdate()
+    {
+        Quaternion currentRotations = this.gameObject.transform.rotation;
+        float delta = Quaternion.Angle(currentRotations, initialQuarternion);
+        Debug.Log(delta);
+        if(delta >= deltaE)
+        {
+            objectRenderer.material.SetColor("_Color",Color.red);
+        }
+    }
+}
