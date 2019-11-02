66f55bf7a0070bbe79d11ae85aebb8620a88085d
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-11-02 13:56:22 +0000

    cleaned up some of the can code

--- Assets/Scripts/Knockable Minigame/Knockable.cs
+++ Assets/Scripts/Knockable Minigame/Knockable.cs
@@ -7,20 +7,16 @@ public class Knockable : MonoBehaviour
     public float deltaE = 5;
 
     private Quaternion initialQuarternion;
-    private Renderer objectRenderer;
 
     public delegate void NotifyOnKnockdownDelegate();
-
     List<NotifyOnKnockdownDelegate> registeredDelegates = new List<NotifyOnKnockdownDelegate>();
 
     private bool KnockedDown = false;
-
     private AudioSource canSound;
 
     void Start()
     {
         canSound = GetComponent<AudioSource>();
-        objectRenderer = GetComponent<Renderer>();
         initialQuarternion = this.gameObject.transform.rotation;
     }
 
@@ -32,7 +28,6 @@ public class Knockable : MonoBehaviour
             float delta = Quaternion.Angle(currentRotations, initialQuarternion);
             if (delta >= deltaE)
             {
-                objectRenderer.material.SetColor("_Color", Color.red);
                 foreach (NotifyOnKnockdownDelegate handler in registeredDelegates)
                 {
                     KnockedDown = true;
@@ -45,8 +40,8 @@ public class Knockable : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         Vector3 impulse = collision.impulse;
-        float delay = Random.Range(0.001f, 0.005f);
-        //canSound.volume = impulse.normalized.magnitude/Vector3.one.magnitude;
+        float delay = Random.Range(0.01f, 0.09f);
+        canSound.volume = impulse.normalized.magnitude/Vector3.one.magnitude;
         canSound.PlayDelayed(delay);
     }
 
